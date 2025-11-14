cdbac5444bc74e61529e1d5fa6ed306a982b4856
DrissiOmar <omar.drissi@esprit.tn>
2025-11-14 13:24:15 +0100

    refactor(user): organize User module by feature (domain, DTOs, mappers, service, repository)

---
+++ Backend-Modulith/src/main/java/com/omar/tickets/user/handler/UserExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.omar.tickets.user.handler;
+
+import com.omar.tickets.shared.dtos.ErrorDto;
+import com.omar.tickets.user.exceptions.UserNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class UserExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleUserNotFoundException(UserNotFoundException ex) {
+        log.error("Caught UserNotFoundException", ex);
+        ErrorDto errorDto = new ErrorDto();
+        errorDto.setError("User not found");
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
+}
